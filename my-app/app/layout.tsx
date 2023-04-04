af4d49dd453788d140acc71ea6491a83025ad621
유영빈 <97148877+BTSBRINGMEHERE@users.noreply.github.com>
2023-04-04 16:41:33 +0900

    다른 방법의 nextjs 공부
    기존에 pages에서 페이지 관리를 했다면
    app 디렉토리로 바꿔서 페이지 관리를 했습니다.

---
+++ my-app/app/layout.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../styles/globals.css";
+import Header from "./Header";
+
+// page 관리를 해당 컴포넌츠에서 실행할 예정이라서 pages에 있던 index, _document를 제거한 상태임
+// next.config.js 폴더에 experimental: {appDir: true} 앱 디렉토리를 사용한다고 바꿨기 때문임
+// 기존의 pages에서 이런 저런 페이지 설정을 했지만 appDir에서 하는 강좌이기 때문에 바꿈
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <head></head>
+      <body>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ my-app/app/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// 해당 디렉토리의 페이지 이름은 무조건 page로 설정 해당 URL의 메인페이지라 생각하면 됨
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
