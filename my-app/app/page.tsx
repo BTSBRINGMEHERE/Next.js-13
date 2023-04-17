79d4e4760ba9c74bdba395e335039f22ad891d4b
유영빈 <97148877+BTSBRINGMEHERE@users.noreply.github.com>
2023-04-17 16:12:35 +0900

    commit
    commit

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -6,7 +6,6 @@ const Home = () => {
   return (
     <div>
       <Suspense fallback={<p>Loading the Todos...</p>}>
-        <h1>Loading Todos</h1>
         <div className="flex space-x-2">
           {/*@ts-ignore */}
           <TodosList />
@@ -14,7 +13,6 @@ const Home = () => {
       </Suspense>
 
       <Suspense fallback={<p>Loading the Shopping Trolley...</p>}>
-        <h1>Loading Shopping Trolley</h1>
         <div className="flex space-x-2">
           {/*@ts-ignore */}
           <TodosList />
